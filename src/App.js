abe6f2fb13425408b4ae46a5e815032ffb050d9d
Gem <mojeedibrahim13@gmail.com>
2025-11-30 23:58:25 -0800

    componet file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 import './App.css';
+import Header from './components/Header';
+import MobileNav from './components/MobileNav';
+import Hero from './components/Hero';
+import Stats from './components/Stats';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header/>
+      <MobileNav/>
+      <Hero/>
+      <Stats/>
+      <CTA/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+// import navLogo from "../assets/images/logo.svg";
+
+const Header = () => {
+
+    return(
+        <></>
+    );
+}
+
+export default Header;
